8d7bc8ebaca4c98b7daccd569407069a6f84bfbc
rohit-rajput-1 <rohit31rajput2001@gmail.com>
2024-06-22 21:39:59 +0530

    views modified

--- rangam_ai/views.py
+++ rangam_ai/views.py
@@ -1,8 +1,8 @@
 from rest_framework import views,status
 from rest_framework.response import Response
 from django.contrib.auth.models import User
-from rangam_ai.serializers import JobDescriptionSerializer,UserSerializer,TokenSerializer
-from rangam_ai.models import JobDescription
+from rangam_ai.serializers import JobDescriptionSerializer,UserSerializer,TokenSerializer, JobSkillsRequiredSerializer,JobQuestionsRequiredSerializer ,JobKeyResponsibilitiesRequiredSerializer
+from rangam_ai.models import JobDescription,JobSkillsRequired, JobQuestionsRequired, JobKeyResponsibilitiesRequired
 from rest_framework.authtoken.views  import ObtainAuthToken
 from rest_framework.authentication import TokenAuthentication
 
@@ -25,6 +25,56 @@ class JobDescriptionView(views.APIView):
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
+class JobSkillsRequiredView(views.APIView):
+    serializer_class = JobSkillsRequiredSerializer
+    authentication_classes = [TokenAuthentication]
+
+    def get(self, request, format=None):
+        qs = JobSkillsRequired.objects.all()
+        serializer = self.serializer_class(qs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class JobQuestionsRequiredView(views.APIView):
+    serializer_class = JobQuestionsRequiredSerializer
+    authentication_classes = [TokenAuthentication]
+
+    def get(self, request, format=None):
+        qs = JobQuestionsRequired.objects.all()
+        serializer = self.serializer_class(qs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class JobKeyResponsibilitiesRequiredView(views.APIView):
+    serializer_class = JobKeyResponsibilitiesRequiredSerializer
+    authentication_classes = [TokenAuthentication]
+
+    def get(self, request, format=None):
+        qs = JobKeyResponsibilitiesRequired.objects.all()
+        serializer = self.serializer_class(qs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 # Now we will create the User View to handle, When the client make request to user endpoint.
 class UserView(views.APIView):
