4b6347ce78b2aa8f1f941ad544e961610cc62499
rohit-rajput-1 <rohit31rajput2001@gmail.com>
2024-06-22 21:37:32 +0530

    model modified

--- rangam_ai/models.py
+++ rangam_ai/models.py
@@ -1,10 +1,29 @@
 from django.db import models
 
-# Here we will put the input code as Job Description and the output code will be the extracted key responsibilities from the job description using the GPT-3.5 model.
-
 class JobDescription(models.Model):
     _input_job_description = models.TextField()
     _output_response = models.TextField()
 
     class Meta:
         db_table = "job_description"
+
+class JobSkillsRequired(models.Model):
+    _input_job_description = models.TextField()
+    _output_response = models.TextField()
+
+    class Meta:
+        db_table = "job_skills_required"
+
+class JobQuestionsRequired(models.Model):
+    _input_job_description = models.TextField()
+    _output_response = models.TextField()
+
+    class Meta:
+        db_table = "job_questions_required"
+
+class JobKeyResponsibilitiesRequired(models.Model):
+    _input_job_description = models.TextField()
+    _output_response = models.TextField()
+
+    class Meta:
+        db_table = "job_key_responsibilities_required"
