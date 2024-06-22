76fcc661dc6aebd334abfbc453904bad338a654e
rohit-rajput-1 <rohit31rajput2001@gmail.com>
2024-06-22 21:38:58 +0530

    urls endpoints added

--- rangam_ai/urls.py
+++ rangam_ai/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from rangam_ai.views import UserView,TokenView,JobDescriptionView
+from rangam_ai.views import UserView,TokenView,JobDescriptionView,JobSkillsRequiredView, JobQuestionsRequiredView, JobKeyResponsibilitiesRequiredView
 urlpatterns = [
     path('users/',UserView.as_view(),name='users'),
     path('tokens/',TokenView.as_view(),name='tokens'),
     path('job_description/',JobDescriptionView.as_view(),name='job_description'),
+    path('job-skills-required/', JobSkillsRequiredView.as_view(), name='job-skills-required'),
+    path('job-questions-required/', JobQuestionsRequiredView.as_view(), name='job-questions-required'),
+    path('job-key-responsibilities-required/', JobKeyResponsibilitiesRequiredView.as_view(), name='job-key-responsibilities-required'),
 ]
