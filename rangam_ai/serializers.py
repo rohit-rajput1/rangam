05fb0eb8a2e0b0b38ecdebb1aabc9017e8fd9f8f
rohit-rajput-1 <rohit31rajput2001@gmail.com>
2024-06-22 11:21:23 +0530

    serializer file modified

--- rangam_ai/serializers.py
+++ rangam_ai/serializers.py
@@ -4,46 +4,64 @@ from django.contrib.auth.models import User
 from rangam_ai.utils import send_job_description_to_api
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
+
 class JobDescriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = JobDescription
-        fields = ("id","_input_job_description","_output_response")
+        fields = ("id", "_input_job_description", "_output_response")
         extra_kwargs = {"_output_response": {"read_only": True}}
 
-    def create(self,validated_data):
-        jd = validated_data(**validated_data)
+    def create(self, validated_data):
+        # Initialize a new JobDescription object with the validated data
+        jd = JobDescription(**validated_data)
+        
+        # Send the input job description to the external API and get the response
         _output_response = send_job_description_to_api(validated_data["_input_job_description"])
+        
+        # Set the output response on the JobDescription object
         jd._output_response = _output_response
+        
+        # Save the JobDescription object to the database
         jd.save()
+        
+        # Return the saved JobDescription object
         return jd
-    
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id","username","email","password")
+        fields = ("id", "username", "email", "password")
         extra_kwargs = {"password": {"write_only": True}}
     
-    def create(self,validated_data):
+    def create(self, validated_data):
+        # Extract the password from validated data
         password = validated_data.pop("password")
-        user = User.obejcts.create(**validated_data)
-        # So this save the user password as hashed and encrypted
+        
+        # Create a new User object with the remaining data
+        user = User.objects.create(**validated_data)
+        
+        # Set the user's password (hashing it)
         user.set_password(password)
+        
+        # Save the user object to the database
         user.save()
-        # So we creating the user for the token based authentication
+        
+        # Create an authentication token for the user
         Token.objects.create(user=user)
+        
+        # Return the newly created user object
         return user
 
-# Now we are creating the Token Serializer to handle th
-class TokenSerializer(serializers.ModelSerializer):
+class TokenSerializer(serializers.Serializer):
     username = serializers.CharField()
-    password = serializers.CharField(style={"input_type": "password"},trim_whitespace=False)
+    password = serializers.CharField(style={"input_type": "password"}, trim_whitespace=False)
 
-    def validate(self,attrs):
+    def validate(self, attrs):
         username = attrs.get("username")
         password = attrs.get("password")
-        user = authenticate(request=self.context.get("request"),username=username,password=password)
+        user = authenticate(request=self.context.get("request"), username=username, password=password)
         if not user:
             msg = "Unable to authenticate with provided credentials"
-            raise serializers.ValidationError(msg,code="authorization")
+            raise serializers.ValidationError(msg, code="authorization")
         attrs["user"] = user
         return attrs
